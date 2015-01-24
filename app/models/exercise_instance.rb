08ef65a8c7ccdf30d9dd8c1e7edf660f90d8c76c
Aaron Snyder <a453803@gmail.com>
2015-01-23 22:47:54 -0700

    general UX cleanup

--- app/models/exercise_instance.rb
+++ app/models/exercise_instance.rb
@@ -3,5 +3,5 @@ class ExerciseInstance < ActiveRecord::Base
  	belongs_to :exercise
  	validates :station_id, presence: true
   	validates :exercise_id, presence: true
-  	default_scope {order('created_at desc')}
+  	default_scope {order('created_at asc')}
 end
